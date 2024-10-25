d8b768fb4381771a6512ea709670502f25595f8c
NathanPERIER <periernathan2000@gmail.com>
2024-10-26 01:38:55 +0200

    Added first version of the Fourier transform (not optimised)

---
+++ lib/fcircles/fourier_transform.hh
@@ -0,0 +1,35 @@
+
+#pragma once
+
+#include <vector>
+
+#include <npp/math/complex.hh>
+
+
+namespace fcl {
+
+std::vector<npp::complex> fourier_transform(std::vector<npp::complex> points) {
+    std::vector<npp::complex> res;
+    for(uint64_t k = 0; k < points.size(); k++) {
+        npp::complex x = 0;
+        for(uint64_t n = 0; n < points.size(); n++) {
+            x += points[n] * npp::complex::polar(1, -2.0 * M_PI * static_cast<double>(k) * static_cast<double>(n) / static_cast<double>(points.size()));
+        }
+        res.push_back(x);
+    }
+    return res;
+}
+
+std::vector<npp::complex> inverse_fourier_transform(std::vector<npp::complex> points) {
+    std::vector<npp::complex> res;
+    for(uint64_t n = 0; n < points.size(); n++) {
+        npp::complex x = 0;
+        for(uint64_t k = 0; k < points.size(); k++) {
+            x += points[k] * npp::complex::polar(1, 2.0 * M_PI * static_cast<double>(k) * static_cast<double>(n) / static_cast<double>(points.size()));
+        }
+        res.push_back(x / static_cast<double>(points.size()));
+    }
+    return res;
+}
+
+} // namespace fcl
